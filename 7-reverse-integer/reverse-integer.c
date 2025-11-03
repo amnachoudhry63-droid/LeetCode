b347842f8eeae19ebde79f366d191394d8d52b9e
amnachoudhry63-droid <amnachoudhry63@gmail.com>
2025-11-03 22:59:47 +0500

    Time: 3 ms (53.68%) | Memory: 8.3 MB (98.27%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,17 @@
+
+#include <limits.h>
+
+int reverse(int x) {
+    long rev = 0;
+    while (x != 0) {
+        int digit = x % 10;
+        x = x / 10;
+        rev = rev * 10 + digit;
+
+        if (rev > INT_MAX || rev < INT_MIN) {
+            return 0;
+        }
+    }
+    return (int)rev;
+}
+
